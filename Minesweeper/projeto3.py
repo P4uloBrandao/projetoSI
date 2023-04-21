f776f255fa080960ce7891ca0863e4676b610ed5
PauloBrandao <paulo.brandao@visualnuts.com>
2023-04-21 15:31:00 +0100

    .

---
+++ Minesweeper/projeto3.py
@@ -0,0 +1,57 @@
+from csp import *
+from p3_aux import *
+
+
+def getVariables(puzzle):
+    variables = []
+    rows, cols = len(puzzle), len(puzzle[0])
+    for x in range(rows):
+        for y in range(cols):
+            if type(puzzle[x][y]) == int:
+                varName= "V_%d_%d" % (x, y)
+                if varName not in variables:
+                    variables.append("V_%d_%d" % (x, y))
+            else:
+                neighbors= getNeighbors(x, y, rows, cols)
+                for j in neighbors:
+                    if type(puzzle[j[0]][j[1]]) == int:
+                        varName= "V_%d_%d" % (j[0], j[1])
+                        if varName not in variables:
+                            variables.append("V_%d_%d" % (j[0], j[1]))
+    variables = sorted(variables)
+    return variables
+
+
+def getNeighbors(x, y, rows, cols):
+    neighbors = []
+    if x - 1 >= 0:
+        neighbors.append((x-1, y))
+    if x + 1 < rows:
+        neighbors.append((x+1, y))
+    if y - 1 >= 0:
+        neighbors.append((x, y-1))
+    if y + 1 < cols:
+        neighbors.append((x, y+1))
+    if x - 1 >= 0 and y - 1 >= 0:
+        neighbors.append((x-1, y-1))
+    if x - 1 >= 0 and y + 1 < cols:
+        neighbors.append((x-1, y+1))
+    if x + 1 < rows and y - 1 >= 0:
+        neighbors.append((x+1, y-1))
+    if x + 1 < rows and y + 1 < cols:
+        neighbors.append((x+1, y+1))
+    return neighbors
+
+
+puzzle = [[1, 2, '#', '#', '#'],
+          [1, '#', '#', '#', 2],
+          ['#', '#', '#', 4, '#'],
+          ['#', '#', 2, '#', 3],
+          [2, 2, '#', 2, '#']]
+
+print(getVariables(puzzle))
+
+
+def minesweeper_CSP(puzzle):
+    variables = getVariables(puzzle)
+    return minesweeper_CSP(puzzle)
