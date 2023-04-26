0673a9ea68b2b5b2710c992ea18cd897f03c2d71
PauloBrandao <paulo.brandao@visualnuts.com>
2023-04-26 23:28:03 +0100

    add domains

--- Minesweeper/projeto3.py
+++ Minesweeper/projeto3.py
@@ -8,14 +8,14 @@ def getVariables(puzzle):
     for x in range(rows):
         for y in range(cols):
             if type(puzzle[x][y]) == int:
-                varName= "V_%d_%d" % (x, y)
+                varName = "V_%d_%d" % (x, y)
                 if varName not in variables:
                     variables.append(varName)
             else:
-                neighbors= getNeighbors(x, y, rows, cols)
+                neighbors = getNeighbors(x, y, rows, cols)
                 for j in neighbors:
                     if type(puzzle[j[0]][j[1]]) == int:
-                        varName= "V_%d_%d" % (x, y)
+                        varName = "V_%d_%d" % (x, y)
                         if varName not in variables:
                             variables.append(varName)
     variables = sorted(variables)
@@ -43,15 +43,51 @@ def getNeighbors(x, y, rows, cols):
     return neighbors
 
 
+def defineDomains(puzzle):
+    domain = {}
+    variaveis = getVariables(puzzle)
+    rows, cols = len(puzzle), len(puzzle[0])
+    for var in variaveis:
+        varParsed = int(var.split('_')[1]), int(var.split('_')[2])
+        if puzzle[varParsed[1]][varParsed[0]] == "#":
+            domain[var] = [0, 1]
+        else:
+            neighbors = getNeighbors(varParsed[1], varParsed[0], rows, cols)
+            espacosVazios = 0
+            numBombas = int(puzzle[varParsed[1]][varParsed[0]])
+            lst = []
+            for x in neighbors:
+                if puzzle[x[0]][x[1]] == "#":
+                    espacosVazios += 1
+            for x in range(espacosVazios-1, -1, -1):
+                lst.append(x)
+            res=[]
+            for comb in itertools.combinations(lst, numBombas):
+                newLst = [0] * espacosVazios
+                for x in comb:
+                    newLst[x] = 1
+                res.append(tuple(newLst))
+            domain[var]=res
+
+    return domain
+
+
 puzzle = [[1, 2, '#', '#', '#'],
           [1, '#', '#', '#', 2],
           ['#', '#', '#', 4, '#'],
           ['#', '#', 2, '#', 3],
           [2, 2, '#', 2, '#']]
 
-print(getVariables(puzzle))
+print(defineDomains(puzzle))
 
 
 def minesweeper_CSP(puzzle):
+    # Definir Variáveis
     variables = getVariables(puzzle)
-    return minesweeper_CSP(puzzle)
+    # Definir Domínios
+    domains = defineDomains(puzzle)
+
+
+    return CSP(variables, domains, neighbors ,constraints)
+
+
