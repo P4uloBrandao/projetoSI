24632bd03e51f4b04dde23b265c7e54f2e0c9d19
PauloBrandao <paulo.brandao@visualnuts.com>
2023-04-28 01:37:45 +0100

    .

--- Minesweeper/projeto3.py
+++ Minesweeper/projeto3.py
@@ -31,22 +31,23 @@ def show_domains(domains):
 
 def getNeighbors(x, y, rows, cols):
     neighbors = []
-    if x - 1 >= 0:
-        neighbors.append((x-1, y))
-    if x + 1 < rows:
-        neighbors.append((x+1, y))
     if y - 1 >= 0:
         neighbors.append((x, y-1))
-    if y + 1 < cols:
-        neighbors.append((x, y+1))
-    if x - 1 >= 0 and y - 1 >= 0:
-        neighbors.append((x-1, y-1))
-    if x - 1 >= 0 and y + 1 < cols:
-        neighbors.append((x-1, y+1))
     if x + 1 < rows and y - 1 >= 0:
         neighbors.append((x+1, y-1))
+    if x + 1 < rows:
+        neighbors.append((x+1, y))
     if x + 1 < rows and y + 1 < cols:
         neighbors.append((x+1, y+1))
+    if y + 1 < cols:
+        neighbors.append((x, y+1))
+    if x - 1 >= 0 and y + 1 < cols:
+        neighbors.append((x-1, y+1))
+    if x - 1 >= 0:
+        neighbors.append((x-1, y))
+
+    if x - 1 >= 0 and y - 1 >= 0:
+        neighbors.append((x-1, y-1))
     return neighbors
 
 
@@ -75,16 +76,46 @@ def defineDomains(puzzle):
                     newLst[x] = 1
                 res.append(tuple(newLst))
             domain[var] = sorted(res)
-            
 
     return domain
 
 
-puzzle = [[1, 2, '#', '#', '#'],
-          [1, '#', '#', '#', 2],
-          ['#', '#', '#', 4, '#'],
-          ['#', '#', 2, '#', 3],
-          [2, 2, '#', 2, '#']]
+def defineConstraint(A, a, B, b):
+    if type(a) == int:
+        positianA = (int(A.split('_')[1]), int(A.split('_')[2]))
+        positianB = (int(B.split('_')[1]), int(B.split('_')[2]))
+        neighbors = getNeighbors(
+            positianB[1], positianB[0], len(puzzle), len(puzzle[0]))
+        vazios = []
+        for pos in range(len(neighbors)):
+            if puzzle[neighbors[pos][1]][neighbors[pos][0]] == "#":
+                vazios.append(neighbors[pos])
+        for x in range(len(vazios)):
+            if vazios[x] == positianA:
+                if len(b) >= x+1:
+                    return a == b[x]
+
+    if type(b) == int:
+        positianA = (int(A.split('_')[1]), int(A.split('_')[2]))
+        positianB = (int(B.split('_')[1]), int(B.split('_')[2]))
+        neighbors = getNeighbors(
+            positianA[1], positianA[0], len(puzzle), len(puzzle[0]))
+        vazios = []
+        for pos in range(len(neighbors)):
+            if puzzle[neighbors[pos][1]][neighbors[pos][0]] == "#":
+                vazios.append(pos)
+        for x in range(len(vazios)):
+            if vazios[x] == positianB:
+                if len(a) >= x+1:
+                    return a[x] == b
+    return a == b
+
+
+# puzzle = [[1, 2, '#', '#', '#'],
+#           [1, '#', '#', '#', 2],
+#           ['#', '#', '#', 4, '#'],
+#           ['#', '#', 2, '#', 3],
+#           [2, 2, '#', 2, '#']]
 
 
 def minesweeper_CSP(puzzle):
@@ -97,10 +128,28 @@ def minesweeper_CSP(puzzle):
     for var in variables:
         varParsed = int(var.split('_')[1]), int(var.split('_')[2])
         neighbors[var] = []
-        for x in getNeighbors(varParsed[1], varParsed[0], len(puzzle), len(puzzle[0])):
-            neighbors[var].append("V_%d_%d" % (x[0], x[1]))
 
-    return CSP(variables, domains, neighbors, different_values_constraint)
+        if type(puzzle[varParsed[1]][varParsed[0]]) == int:
+            for x in getNeighbors(varParsed[0], varParsed[1], len(puzzle), len(puzzle[0])):
+                if puzzle[x[1]][x[0]] == "#":
+                    neighbors[var].append("V_%d_%d" % (x[0], x[1]))
+            neighbors[var] = sorted(neighbors[var])
+        else:
+            for x in getNeighbors(varParsed[0], varParsed[1], len(puzzle)-1, len(puzzle[0])-1):
+                if type(puzzle[x[1]][x[0]]) == int:
+                    neighbors[var].append("V_%d_%d" % (x[0], x[1]))
+            neighbors[var] = sorted(neighbors[var])
+    print(neighbors["V_4_1"])
+    
+    return CSP(variables, domains, neighbors, defineConstraint)
 
 
-print(show_domains(defineDomains(puzzle)))
+
+puzzle = [[1, 2, '#', '#', '#', '#', '#'],
+            [1, '#', '#', '#', 2, '#', '#'],
+            ['#', '#', '#', 4, '#', '#', '#'],
+            ['#', '#', 2, '#', 3, '#', '#'],
+            [2, 2, '#', 2, '#', '#', '#'],
+            ['#', '#', '#', '#', '#', '#', '#']]
+xxx = minesweeper_CSP(puzzle)
+grafo = xxx.neighbors
